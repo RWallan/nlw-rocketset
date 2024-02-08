d037c25d1411b6de7d558aa6f4ae07abe0f6c4c8
RWallan <3am.richardwallan@gmail.com>
2024-02-08 20:04:12 -0300

    refac: Implementando o controller de barcode

--- nlw_rocketset/views/tag_creator_view.py
+++ nlw_rocketset/views/tag_creator_view.py
@@ -1,8 +1,18 @@
+from nlw_rocketset.controllers.tag_creator_controller import (
+    TagCreateController,
+)
 from nlw_rocketset.views.http_types.http_request import HttpRequest
 from nlw_rocketset.views.http_types.http_response import HttpResponse
 
 
 class TagCreatorView:
-    def validate_and_create(self, http_request: HttpRequest):
+    def __init__(self) -> None:
+        self.tag_creator_controller = TagCreateController()
 
-        return HttpResponse(status_code=200, body={"hello": "world"})
+    def validate_and_create(self, http_request: HttpRequest):
+        body = http_request.body
+        product_code = body["product_code"]
+
+        formatted_response = self.tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
